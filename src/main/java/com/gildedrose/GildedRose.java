d0a4f60d4ac2d43c667f937173757e89f3c75fbc
Ayman Ahmed Ebrahim <ayman.ahmed.ebrahim95@gmail.com>
2022-02-06 19:36:38 +0200

    Refactor:extract Sulfuras from update quality logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,11 +9,13 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                continue;
+            }
+
             if (!item.name.equals("Aged Brie")
                     && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    updateNormalItem(item);
-                }
+                updateNormalItem(item);
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
@@ -34,20 +36,14 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
+
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                //item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = item.quality - item.quality;
+
                     }
                 } else {
                     if (item.quality < 50) {
